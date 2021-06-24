c2223887aac4ceef18bff53db7eed1c477c45ec9
kaustubha-chaturvedi <kaustubhachaturvedi@outlook.com>
2021-06-24 09:57:26 +0530

    wsgi settings

--- pyBlog/wsgi.py
+++ pyBlog/wsgi.py
@@ -7,10 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+sys.path.append('/var/www/app/pyBlog')
+sys.path.append('/var/www/app/app/Lib/site-packages')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pyBlog.settings')
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pyBlog.settings')
 
 application = get_wsgi_application()
